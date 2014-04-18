20148f24477f9eb8f3a1ba5d6300eed6fca890c9
flayumu@gmail.com <flayumu@gmail.com@13f746cb-05e7-7ff6-345a-8f3762072e29>
2014-04-18 09:18:32 +0000

    เพิ่มให้เวลาพิมแล้ว enter ไปตรงบริเวรนั้นได้

--- GoogleMapsApp/public_html/javascript/GoogleMapsProcess.js
+++ GoogleMapsApp/public_html/javascript/GoogleMapsProcess.js
@@ -13,11 +13,89 @@ function initialize() {
   };
    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
    directionsDisplay.setMap(map);
+   
+  markers = [];
+  
   var input = document.getElementById('address');
   var input2 = document.getElementById('address2');
-  var searchBox = new google.maps.points.SearchBox((input));
-  var searchBox2 = new google.maps.points.SearchBox(input2);
+  var searchBox = new google.maps.places.SearchBox(input);
+  var searchBox2 = new google.maps.places.SearchBox(input2);
+  var allsearchbox = new Array();
+  
 
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location. 
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+  
+  google.maps.event.addListener(searchBox2, 'places_changed', function() {
+    var places = searchBox2.getPlaces();
+
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location. 
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+    
+  /*google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+      });*/
+  
   google.maps.event.addListener(map, 'click', function showAlert(event) {
 	    placeMarker(event.latLng,map);
 	    window.alert("You have clicked on \nLatitude : "+event.latLng.lat().toString()
@@ -61,6 +139,7 @@ function placeMarker(position,map){
 function clearDirection() {
   directionsDisplay.setMap(null);
   initialize();
+  point = [];
   count = 1;
 }
 
