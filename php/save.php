221d10de23e44aa9e335badf49c8b459008e4774
ripzery@gmail.com <ripzery@gmail.com@13f746cb-05e7-7ff6-345a-8f3762072e29>
2014-04-24 02:35:32 +0000

    1.เพิ่ม field ใน database คือ {route_type,date} 2.ปรับการส่งค่าขึ้น database ในไฟล์ save.php (สร้างการดึงเวลาจากตัวเครื่องและรับค่าroute_typeจาก js) 3.ปรับการ Load ใหม่โดยดึงข้อมูลเพิ่ม 2 fields และ สร้าง array สำหรับเก็บ name,route_type,date,waypoint ต่างๆแต่ละแถว 4.สร้าง dialog เมื่อกดปุ่ม load จะสร้าง dialog ขึ้นมาโดยดึงค่า name,date จาก database ขึ้นมาโชว์ใน scrollable listbox ทั้งหมด รวมถึงสามารถ search ได้โดยมีระบบ autocomplete อิงค่าจากชื่อไฟล์ทั้งหมด (ยัง sortข้อมูลไม่ได้,ยังดึงข้อมูลมาจริงๆไม่ได้ โดยรวมแล้วฟังก์ชั่นโหลดยังทำงานไม่ได้) 5.แก้การ selector element โดยเปลี่ยนไปใช้ jquery ที่มีการอ้างอิง element ที่เฉพาะเจาะจงกว่า ทำให้เพิ่ม tag ใดๆมาก็ไม่มีผลต่อการทำงานของโค้ดอีกแล้ว (ถ้าไม่ใส่ id หรือ class ซ้ำกับอันที่มีอยู่แล้ว)

--- php/save.php
+++ php/save.php
@@ -1,10 +1,14 @@
 <?php
     $filename = $_POST['name'];
-    $command = $_POST['command'];
+    $route_type = $_POST['route_type'];
     $latlng  = $_POST['latlng'];   
     $pos_value = "";
     $i=0;
     $pos_field="";
+    
+    date_default_timezone_set('Thailand/Bangkok');
+    $date = date('Y-m-d', time());
+    
     foreach($latlng as $lalong){
         $i++;
         if($i === count($latlng)){
@@ -20,7 +24,7 @@
     {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-    if(mysqli_query($sql, "INSERT INTO `google-maps` (`name`,`command`,$pos_field) VALUES ('$filename','$command',$pos_value);"))
+    if(mysqli_query($sql, "INSERT INTO `google-maps` (`name`,`route_type`,`date`,$pos_field) VALUES ('$filename','$route_type','$date',$pos_value);"))
             echo "record successful";
     else{
         echo mysqli_error($sql);
