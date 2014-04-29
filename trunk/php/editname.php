5f89143d597795b60941a2af2183c6d833d55c61
flayumu@gmail.com <flayumu@gmail.com@13f746cb-05e7-7ff6-345a-8f3762072e29>
2014-04-29 07:44:39 +0000

    CSS -> จัดหน้าเว็บ PHP -> เพิ่มให้แก้ชื่อในตารางแล้ว edit date ได้ แต่ยังไม่ update เรียลทาม JS -> เพิ่มให้หน้า load สามารถเรียงข้อมูลจากมากไปน้อยและน้อยไปมากสลับกันได้

--- trunk/php/editname.php
+++ trunk/php/editname.php
@@ -1,7 +1,9 @@
 <?php
 $name = filter_input(INPUT_POST,'newvalue');
 $origin_name = filter_input(INPUT_POST,'origValue');
-$sql = mysqli_connect("localhost","root","rabarip","maps");
-mysqli_query($sql, "UPDATE `google-maps` SET `name` = '".$name."' WHERE `name` = '".$origin_name."'");
+date_default_timezone_set('Thailand/Bangkok');
+$date = date('Y-m-d', time());
+$sql = mysqli_connect("localhost","root","first1209","maps");
+mysqli_query($sql, "UPDATE `google-maps` SET `name` = '".$name."' , `date` = '".$date."' WHERE `name` = '".$origin_name."'");
 mysqli_close($sql);
 echo $name;
