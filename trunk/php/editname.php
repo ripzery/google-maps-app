3772d90828533298f5dbcfa30b29a05dcb8d370c
ripzery@gmail.com <ripzery@gmail.com@13f746cb-05e7-7ff6-345a-8f3762072e29>
2014-04-30 09:46:39 +0000

    1. minor change ลืมไปละ

--- trunk/php/editname.php
+++ trunk/php/editname.php
@@ -3,7 +3,7 @@ $name = filter_input(INPUT_POST,'newvalue');
 $origin_name = filter_input(INPUT_POST,'origValue');
 date_default_timezone_set('Thailand/Bangkok');
 $date = date('Y-m-d', time());
-$sql = mysqli_connect("localhost","root","first1209","maps");
+$sql = mysqli_connect("localhost","root","rabarip","maps");
 mysqli_query($sql, "UPDATE `google-maps` SET `name` = '".$name."' , `date` = '".$date."' WHERE `name` = '".$origin_name."'");
 mysqli_close($sql);
 echo $name;
