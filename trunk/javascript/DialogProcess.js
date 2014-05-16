ee86387662f63205177f0cbef7f5225044a9eddb
ripzery@gmail.com <ripzery@gmail.com@13f746cb-05e7-7ff6-345a-8f3762072e29>
2014-05-16 13:46:57 +0000

    1.เพิ่ม pagination หน้า database

--- trunk/javascript/DialogProcess.js
+++ trunk/javascript/DialogProcess.js
@@ -1,39 +1,40 @@
-$(function() {
+$(function () {
     var direction = $("#direction");
     var load = $("#load");
     var t = $('#t');
     var order = $('#order');
-    
-    $("#guide").click(function(){
-        $(direction).modal({keyboard : true});
+
+    $("#guide").click(function () {
+        $(direction).modal({
+            keyboard: true
+        });
     });
-    
-    $('body').keyup(function(event){
-       if(event.keyCode === 27){
-           $(direction).modal('hide');
-           $(load).modal('hide');
-       } 
+
+    $('body').keyup(function (event) {
+        if (event.keyCode === 27) {
+            $(direction).modal('hide');
+            $(load).modal('hide');
+        }
     });
-     
-    $( "#opener" ).click(function() {
-        $(load).modal();   // initialized with no keyboard
+
+    $("#opener").click(function () {
+        $(load).modal(); // initialized with no keyboard
         $(t).focus();
     });
-    
-    $("#doLoad").click(function(){
+
+    $("#doLoad").click(function () {
         Load();
         $(load).modal('hide');
     });
-    
-    $('#load').on('hidden', function(){
+
+    $('#load').on('hidden', function () {
         $(this).data('modal', null);
     });
-    
-    $("#doClose").click(function(){
+
+    $("#doClose").click(function () {
         $(order).text('Ascending');
         $(t).val("");
         $(load).modal('hide');
-         resetFileName();
+        resetFileName();
     });
 });
-  
